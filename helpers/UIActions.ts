d625da28da9cbb284853963ee436d16dce7c5cb5
dacem <74594956+dacem@users.noreply.github.com>
2023-08-03 13:22:12 +0300

    Added helpers

---
+++ helpers/UIActions.ts
@@ -0,0 +1,34 @@
+import { Page } from '@playwright/test';
+import UIElementActions from './UIElementActions';
+
+export default class UIActions {
+  constructor(private page: Page) {
+    this.elementAction = new UIElementActions(page);
+  }
+
+  private elementAction: UIElementActions;
+
+  /**
+   * Navigate to specified URL
+   * @param URL
+   */
+  public async goto(URL: string): Promise<void> {
+    await this.page.goto(URL, {
+      timeout: 10000,
+      waitUntil: 'load',
+    });
+  }
+
+  /**
+   * Returns the instance of UIElementsActions
+   * @param selector
+   * @returns
+   */
+  public element(selector: string): UIElementActions {
+    return this.elementAction.setElement(selector);
+  }
+
+  public async timeout(seconds: number): Promise<void> {
+    await this.page.waitForTimeout((seconds = 1000));
+  }
+}
